fc24413d3d3d857063cde46812fa766a88bd1437
Mayunk Deepak Kulkarni (mayunkul) <mayunkul@cisco.com>
2018-03-04 03:24:43 +0530

    ble improvements for the day

--- fipy-configure/lib/ble_mode.py
+++ fipy-configure/lib/ble_mode.py
@@ -28,3 +28,17 @@ class BLE()
         #
         deploy_f = boot_serv.characteristic(uuid=b'a234567890123456', value=0)
         pycom.nvs_set('mode', 0)
+        
+        deploy_f_cb = deploy_f.callback(trigger=self.ble.CHAR_WRITE_EVENT | self.ble.CHAR_READ_EVENT, handler=deploy_cb_handler)
+
+
+    def char_cb_handler(chr):
+        # handles write and read requests from the client
+        events = chr.events()
+        if events & self.ble.CHAR_WRITE_EVENT:
+            print("Value written is {}".format(chr.value()))
+
+        else:
+            print("Read requested")
+
+    def
