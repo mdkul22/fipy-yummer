3c1f942e3534de96e18527c36a81c1753cd85c30
Mayunk Deepak Kulkarni (mayunkul) <mayunkul@cisco.com>
2018-03-05 11:55:35 +0530

    "ble mode improvements"

--- fipy-configure/lib/ble_mode.py
+++ fipy-configure/lib/ble_mode.py
@@ -24,12 +24,36 @@ class BLE()
         self.ble.callback(trigger=Bluetooth.CLIENT_CONNECTED | Bluetooth.CLIENT_DISCONNECTED, hander=self.conn_cb)
         self.ble.advertise(True)
         boot_service = self.ble.service(uuid=b'1234567890123456', isprimary=True, nbr_chars=5)
-
-        #
-        deploy_f = boot_serv.characteristic(uuid=b'a234567890123456', value=0)
+        wifi_service = self.ble.service(uuid=b'a234567890123456', isprimary=True, nbr_chars=2)
+        lora_service = self.ble.service(uuid=b'b234567890123456', isprimary=True, nbr_chars=3)
+        lte_service = self.ble.service(uuid=b'c234567890123456', isprimary=True, nbr_chars=2)
+        # characteristic / service declarations
+        deploy_f = boot_serv.characteristic(uuid=b'1000', value=0)
         pycom.nvs_set('mode', 0)
-        
+        # generic services
+        wifi_f = boot_service.characteristic(uuid=b'2000', value=0)
+        pycom.nvs_set('wifi', 0)
+        lora_f = boot_service.characteristic(uuid=b'2001', value=0)
+        pycom.nvs_set('lora', 0)
+        lte_f = boot_service.characteristic(uuid=b'2002', value=0)
+        pycom.nvs_set('lte', 0)
+        # wifi_service chars
+        wifi_ssid = wifi_service.characteristic(uuid=b'3000', value=0)
+        pycom.nvs_set('wifi_ssid', 0)
+        wifi_pass = wifi_service.characteristic(uuid=b'3001', vaue=0)
+        pycom.nvs_set('wifi_pass', 0)
+        # lora_service chars
+        lora_appkey = lora_service.characteristic(uuid=b'4000', value=0)
+        pycom.nvs_set('appkey', 0)
+        lora_appSkey = lora_service.characteristic(uuid=b'4001', value=0)
+        pycom.nvs_set('appSkey', 0)
+        lora_nwkSkey = lora_service.characteristic(uuid=b'4002', value=0)
+        pycom.nvs_set('nwkSkey', 0)
+        # lte_service chars
+
+
         deploy_f_cb = deploy_f.callback(trigger=self.ble.CHAR_WRITE_EVENT | self.ble.CHAR_READ_EVENT, handler=deploy_cb_handler)
+
 
 
     def char_cb_handler(chr):
@@ -38,7 +62,5 @@ class BLE()
         if events & self.ble.CHAR_WRITE_EVENT:
             print("Value written is {}".format(chr.value()))
 
-        else:
+        elif events & self.ble.CHAR_READ_EVENT:
             print("Read requested")
-
-    def
--- fipy-configure/lib/str2nvs.py
+++ fipy-configure/lib/str2nvs.py
@@ -1,13 +1,17 @@
-import pycom
 import binascii
 
 def str2int(string):
     x = binascii.hexlify(string)
     x = int(x, 16)
     x = x + 0x200
-    return hex(x)
+    return x
 
 def int2str(integer):
     integer = integer - 0x200
     integer = hex(integer)
-    return binascii.unhexlify(integer[2:])
+    return binascii.unhexlify(integer[2:-1])
+
+y = str2int("My name is Dan")
+print(y)
+x = int2str(y)
+print(x)
